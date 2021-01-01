bc8a285c8e9f6a7e75a084a52dc1d1d139d57879
kajman <kajmaj87@gmail.com>
2021-01-01 10:13:44 +0100

    Added requirements.txt, refacored download.sh and blackified dedup.py

--- deduplication/dedup.py
+++ deduplication/dedup.py
@@ -5,6 +5,7 @@ import glob
 from tqdm import tqdm
 from rich.console import Console
 from rich.progress import track
+
 console = Console()
 
 TEXT_MIN_LENGHT = 10
@@ -14,37 +15,62 @@ with console.status("[bold green]Loading nlp model...") as status:
     nlp = spacy.load("pl_core_news_md")
 
 data = []
-for f in glob.glob('data/*.json'):
-    with open(f, 'r', encoding="utf8") as json_file:
-        data.extend(json.load(json_file)['results'])
+for f in glob.glob("data/*.json"):
+    with open(f, "r", encoding="utf8") as json_file:
+        data.extend(json.load(json_file)["results"])
 
-#docs = [nlp("To jest jakieś zdanie"), nlp("Zadanie domowe nr 1"), nlp("Mieszkanie w jakiejś dzielnicy")]
+# docs = [nlp("To jest jakieś zdanie"), nlp("Zadanie domowe nr 1"), nlp("Mieszkanie w jakiejś dzielnicy")]
 docs = []
 for d in data:
-    if len(d['comment'])>TEXT_MIN_LENGHT or len(d['text'])>TEXT_MIN_LENGHT:
-        docs.append({
-            'id':d['id'],
-            'nlpt': nlp(d['text']),
-            'text': d['text'],
-            'nlpc': nlp(d['comment']),
-            'comment': d['comment'],
-            'dup': d['is_duplicate'],
-            'verdict': d['current_verdict']
-            })
+    if len(d["comment"]) > TEXT_MIN_LENGHT or len(d["text"]) > TEXT_MIN_LENGHT:
+        docs.append(
+            {
+                "id": d["id"],
+                "nlpt": nlp(d["text"]),
+                "text": d["text"],
+                "nlpc": nlp(d["comment"]),
+                "comment": d["comment"],
+                "dup": d["is_duplicate"],
+                "verdict": d["current_verdict"],
+            }
+        )
 
 
 similarities = []
 for i, dx in track(enumerate(docs), "Processing", len(docs)):
     # nlp = spacy.load("pl_core_news_md"))
-    print("{}/{}".format(i,len(docs)-1))
+    print("{}/{}".format(i, len(docs) - 1))
     for j, dy in enumerate(docs):
-        if j>i:
-            simt = dx['nlpt'].similarity(dy['nlpt']) if len(dx['text'])>TEXT_MIN_LENGHT and len(dy['text'])>TEXT_MIN_LENGHT else 0
-            if simt>0.7 and (dx['verdict'] == 'false' or dx['verdict'] == 'true'):
-                simc = dx['nlpc'].similarity(dy['nlpc']) if len(dx['comment'])>TEXT_MIN_LENGHT and len(dy['comment'])>TEXT_MIN_LENGHT else 0
+        if j > i:
+            simt = (
+                dx["nlpt"].similarity(dy["nlpt"])
+                if len(dx["text"]) > TEXT_MIN_LENGHT
+                and len(dy["text"]) > TEXT_MIN_LENGHT
+                else 0
+            )
+            if simt > 0.7 and (dx["verdict"] == "false" or dx["verdict"] == "true"):
+                simc = (
+                    dx["nlpc"].similarity(dy["nlpc"])
+                    if len(dx["comment"]) > TEXT_MIN_LENGHT
+                    and len(dy["comment"]) > TEXT_MIN_LENGHT
+                    else 0
+                )
                 simt = simt if simt < 1 else 2
-                similarities.append((simt,simc,simt**2+simc**2,dx['id'],"Dup:"+str(dx['dup'])+"_v:"+dx['verdict'],dy['id'],"Dup:"+str(dy['dup'])+"_v:"+dy['verdict'],dx['text'],dy['text'],dx['comment'],dy['comment']))
+                similarities.append(
+                    (
+                        simt,
+                        simc,
+                        simt ** 2 + simc ** 2,
+                        dx["id"],
+                        "Dup:" + str(dx["dup"]) + "_v:" + dx["verdict"],
+                        dy["id"],
+                        "Dup:" + str(dy["dup"]) + "_v:" + dy["verdict"],
+                        dx["text"],
+                        dy["text"],
+                        dx["comment"],
+                        dy["comment"],
+                    )
+                )
 
 print("Done. Total records: {}".format(len(similarities)))
 console.log(sorted(similarities, key=lambda x: x[2])[-500:])
-
--- deduplication/download.sh
+++ deduplication/download.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 
-for p in {1..130}
+if [[ -z "${TOKEN}" ]]; then
+  echo "Please provide TOKEN env variable to use this script"
+  exit 1
+fi
+
+for p in {1..132}
 do
   echo "Fetching page $p"
   # paste real curl below
-  curl --compressed > data/pap_page$p.json
+  curl "https://panel-api.fakehunter.pap.pl/news/crew/expert/news?page=$p" \
+  -H 'authority: panel-api.fakehunter.pap.pl' \
+  -H 'pragma: no-cache' \
+  -H 'cache-control: no-cache' \
+  -H 'sec-ch-ua: "Google Chrome";v="87", " Not;A Brand";v="99", "Chromium";v="87"' \
+  -H 'accept: application/json, text/plain, */*' \
+  -H "authorization: Token $TOKEN" \
+  -H 'sec-ch-ua-mobile: ?0' \
+  -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36' \
+  -H 'origin: https://panel.fakehunter.pap.pl' \
+  -H 'sec-fetch-site: same-site' \
+  -H 'sec-fetch-mode: cors' \
+  -H 'sec-fetch-dest: empty' \
+  -H 'referer: https://panel.fakehunter.pap.pl/' \
+  -H 'accept-language: en-US,en;q=0.9,pl;q=0.8,de-DE;q=0.7,de;q=0.6,ca;q=0.5' \
+  --compressed > data/pap_page$p.json
 done
