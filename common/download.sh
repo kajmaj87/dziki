531900ff716e30d12581991b1e002603a8401a4a
kajman <kajmaj87@gmail.com>
2021-01-02 18:56:26 +0100

    Added some better parametrisation to download.sh

--- common/download.sh
+++ common/download.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
+usage() {
+  echo
+  echo "Example usage:"
+  echo "TOKEN=<copy from headers in news request> $0 deduplication/data pap_news_page"
+  echo 
+  echo "This will result in files pap_news_page0.json, pap_news_page1.json etc in deduplication/data directory"
+}
+
 if [[ -z "${TOKEN}" ]]; then
   echo "Please provide TOKEN env variable to use this script"
+  usage
+  exit 1
+fi
+
+if [[ -z "$1" ]]; then
+  echo "Please provide output directory as first argument"
+  usage
+  exit 1
+fi
+
+if [[ -z "$2" ]]; then
+  echo "Please provide filename prefix as second argument"
+  usage
   exit 1
 fi
 
@@ -24,5 +45,5 @@ do
   -H 'sec-fetch-dest: empty' \
   -H 'referer: https://panel.fakehunter.pap.pl/' \
   -H 'accept-language: en-US,en;q=0.9,pl;q=0.8,de-DE;q=0.7,de;q=0.6,ca;q=0.5' \
-  --compressed > data/pap_page$p.json
+  --compressed > $1/$2$p.json
 done
