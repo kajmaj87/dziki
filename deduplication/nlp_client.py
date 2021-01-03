c6e253f62c75adc8057031fd1523bfce7219f1c0
kajman <kajmaj87@gmail.com>
2021-01-03 14:28:53 +0100

    Added grouping functionality in client

--- deduplication/nlp_client.py
+++ deduplication/nlp_client.py
@@ -12,6 +12,7 @@ console = Console()
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument("-l", "--limit", type=int, default=5000, help="Limit amount of texts sent to server")
 parser.add_argument("-s", "--show", type=int, default=200, help="Max amount of records to show")
+parser.add_argument("-g", "--groups", type=str, default=[], help="Provide group names for grouping similar texts", nargs="+")
 config = parser.parse_args()
 
 def loadDocuments():
@@ -38,6 +39,36 @@ def calculateScore(document):
     commentScore = sim["comment"] if "comment" in sim else 0
     return textScore ** 2 + commentScore ** 2
 
+def createGroups(similarities, groupBy, groupThreshold=1):
+    groups = []
+    def findGroupWithContainingAnyId(groups, id1, id2):
+        for g in groups:
+            if id1 in g or id2 in g:
+                return g
+        return None
+    def addDocumentsToGroup(groups, doc1, doc2):
+        group = findGroupWithContainingAnyId(groups, doc1["id"], doc2["id"])
+        def internalAdd(group, doc1, doc2):
+            cdoc1 = dict(doc1)
+            del cdoc1["id"]
+            cdoc2 = dict(doc2)
+            del cdoc2["id"]
+            group[doc1["id"]] = cdoc1
+            group[doc2["id"]] = cdoc2
+
+        if group is None:
+            group = {}
+            internalAdd(group, doc1, doc2)
+            groups.append(group)
+        else:
+            internalAdd(group, doc1, doc2)
+
+    for s in similarities:
+        if groupBy in s["similarities"] and s["similarities"][groupBy] >= groupThreshold:
+            addDocumentsToGroup(groups, s["docs"][0], s["docs"][1])
+    return groups
+
+
 
 c = http.client.HTTPConnection("localhost", 8080)
 d = {
@@ -49,4 +80,10 @@ d = {
 }
 c.request("POST", "/process", json.dumps(d))
 doc = json.loads(c.getresponse().read())
-console.log(sorted(doc["similarities"], key=lambda x: calculateScore(x))[-config.show:])
+similarities = doc["similarities"]
+if config.show>0:
+    console.log("[bold red] Showing {}/{} of best similarities found".format(min(config.show, len(similarities)), len(similarities)))
+    console.log(sorted(similarities, key=lambda x: calculateScore(x))[-config.show:])
+for g in config.groups:
+    console.log("[bold red] Groups by {}".format(g))
+    console.log(sorted(createGroups(similarities, g), key=len))
